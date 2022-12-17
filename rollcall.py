c14daa3e4f1cf63d0bad56ee902955815d7f5abe
Andy Batey <abatey@cmatrust.net>
2022-12-17 18:48:27 +0000

    Added in chase functions

--- rollcall.py
+++ rollcall.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import time
+import random
 
 import board
 import neopixel
 import numpy as np
 
 # LED strip configuration:
-LED_COUNT   = 144      # Number of LED pixels.
+LED_COUNT   = 372      # Number of LED pixels.
 LED_PIN     = board.D12      # GPIO pin
 LED_BRIGHTNESS = 0.2  # LED brightness
 LED_ORDER = neopixel.GRB # order of LED colours. May also be RGB, GRBW, or RGBW
@@ -41,13 +42,53 @@ def rollcall_cycle(wait):
             strip.show()
             time.sleep(wait)
 
-strip.fill((255,255,255))
-strip.show()
+def single_colour_chase(colour, wait):
+    for p in range(0, LED_COUNT-16, 5):
+        strip[p]=colour
+        strip[p+4]=colour
+        strip[p+8]=colour
+        strip[p+12]=colour
+        strip[p+16]=colour
+        strip.show()
+        time.sleep(wait)
+        strip.fill((0,0,0))
+        strip.show
+
+def dual_colour_chase(colour1, colour2, wait):
+    for p in range(0, LED_COUNT-12, 4):
+        strip[p]=colour1
+        strip[p+4]=colour1
+        strip[p+8]=colour1
+        strip[p+12]=colour1
+        strip[LED_COUNT-p-1]=colour2
+        strip[LED_COUNT-p-5]=colour2
+        strip[LED_COUNT-p-9]=colour2
+        strip[LED_COUNT-p-13]=colour2
+        strip.show()
+        time.sleep(wait)
+        strip.fill((0,0,0))
+        strip.show
+
+
+ 
+#strip.fill((255,255,255))
+#strip.show()
 
 # Main function loop
 
 while True:
 
-    time.sleep(1)
-	
-    rollcall_cycle(0.2)    # 0.2 seconds between colour updates
+    choice = random.randint(1,3)
+
+    if choice == 1:
+         rollcall_cycle(0.2)    # 0.2 seconds between colour updates
+    elif choice == 2:
+         single_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)), random.random()*0.06)
+         single_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)), random.random()*0.06)
+         single_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)), random.random()*0.06)
+         single_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)), random.random()*0.06)
+    elif choice == 3:
+         dual_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),random.random()*0.06)
+         dual_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),random.random()*0.06)
+         dual_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),random.random()*0.06)
+         dual_colour_chase((random.randint(0,255),random.randint(0,255),random.randint(0,255)),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),random.random()*0.06)
